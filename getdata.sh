2daabc74036d8627300eef749c7c24bc07ac5e08
Jamis Johnson <jamismanwaring@gmail.com>
2016-03-25 14:58:46 +0000

    fixes getdata script

--- getdata.sh
+++ getdata.sh
@@ -3,13 +3,13 @@
 mkdir data
 mkdir info
 
-wget --accept txt,xls 
-     --mirror 
-     --progress=dots 
-     --adjust-extension 
-     --convert-links 
-     --backup-converted 
-     --no-parent
+wget --accept txt,xls \
+     --mirror \
+     --progress=dot \
+     --adjust-extension \
+     --convert-links \
+     --backup-converted \
+     --no-parent \
      http://web.mta.info/developers/turnstile.html
 
 mv web.mta.info/developers/data/nyct/turnstile/* data
